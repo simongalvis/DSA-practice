fcbeaa49137de36875c5a2e7cf4387f6c6f6b09f
Simon Galvis <simongalvis9@gmail.com>
2021-02-02 17:27:39 -0700

    Achieve intended output using bult in array

--- practice.js
+++ practice.js
@@ -20,3 +20,32 @@ You can return the answer in any order.
 };
 
 console.log(twoSum([3,2,3], 6))
+
+/*
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+
+var arr = [1,2,3,4,5,7];
+//arr.reverse();
+console.log(arr.reverse());
+*/
+
+var addTwoNumbers = function(l1, l2){
+   var reversed1 = parseInt(l1.reverse().join(''));
+   var reversed2 = parseInt(l2.reverse().join(''));
+
+   var reversedSum = reversed1 + reversed2;
+   //make reversedsum into array reversed
+   var reversedSumArr = [];
+   for(let i = reversedSum.toString().length; i > 0; i--){
+    reversedSumArr.push(parseInt(reversedSum.toString()[i-1]))
+    console.log(reversedSum.toString()[0])
+   }
+
+    return   reversedSumArr;
+}
+
+console.log(addTwoNumbers([9,9,9,9,9,9,9], [9,9,9,9]));
+
