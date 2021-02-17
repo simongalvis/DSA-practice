6d0dff5c767e36e13ae640338150e9a946f0ffd2
Simon Galvis <simongalvis9@gmail.com>
2021-02-16 18:17:57 -0700

    Solve maxSubarraySum using sliding window approach

--- practice.js
+++ practice.js
@@ -230,5 +230,26 @@ function countUniqueValues(arr){
   return uniqueValues;
 }
 
-console.log(countUniqueValues([-2,-1,-1,0,1]))
+//console.log(countUniqueValues([]))
 //console.log('hello')
+
+//Sliding Window 
+/* Write a function called maxSubarraySum which accepts an array of integers and a number called n.
+The function should should calculate the maximum sum of n consecutive elements in the array. */
+
+function maxSubarraySum(arr, num){
+let maxSum = 0;
+let tempSum = 0;
+if(arr.length < num) return null;
+
+  for(let i = 0; i<num; i++){
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for(let i = num; i < arr.length; i++){
+    tempSum = tempSum - arr[i-num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+console.log(maxSubarraySum([1,2,1,1], 3))
