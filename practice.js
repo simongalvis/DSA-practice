5cc597bd22afe7002f1c42012aebb494554b191f
Simon Galvis <simongalvis9@gmail.com>
2021-02-23 23:28:21 -0700

    Install jest and create 1st test

--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
-const LinkedList = require("./SinglyLinkedList")
+const LinkedList = require("./LinkedList/SinglyLinkedList")
 /*
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
