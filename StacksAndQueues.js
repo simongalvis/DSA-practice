077ecbcba56b169d0a18dcf731859349b1bc0563
Simon Galvis <simongalvis9@gmail.com>
2021-03-03 17:15:04 -0700

    Create Queue class with enqueue and print methods

--- StacksAndQueues.js
+++ StacksAndQueues.js
@@ -1,9 +1,9 @@
-class Node{
+/* class Node{
     constructor(value, next){
         this.value = value;
         this.next = next;
     }
-}
+} */
 class Stack{
     constructor(){
         this.top = null;
@@ -55,7 +55,7 @@ let stacky = new Stack();
 //stacky.print();
 
 
-stacky.push(77);
+/* stacky.push(77);
 stacky.push(78);
 stacky.push(79);
 stacky.push(80);
@@ -70,3 +70,85 @@ stacky.pop();
 
 console.log("New Stack Here")
 stacky.print();
+ */
+
+
+
+ //Queue
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue{
+    constructor(){
+        this.front = null;
+        this.back = null;
+    }
+    //enqueue - add item to queue
+    enqueue(data){
+        if(this.front === null){
+            this.front = new Node(data, null);
+        }
+        else if(this.back === null){
+            let newNode = new Node(data, null);
+            this.back = newNode;
+            this.front.next = newNode;
+
+        }
+        else{
+            let newNode = new Node(data, null);
+            this.back.next = newNode;
+            this.back = newNode;
+        }
+    }
+    //dequeue - remove an item from the queue
+    //print
+    print(){
+        if(this.first === null) return null;
+        let output = "";
+        let current = this.front;
+        while(current){
+            current.next != null ? output+= current.value + " | " : output+= current.value;
+            current = current.next;
+        }
+        console.log(output);
+    }
+}
+let q = new Queue();
+
+
+q.enqueue(77);
+q.enqueue(78);
+q.enqueue(79);
+q.enqueue(80);
+q.enqueue(81);
+q.enqueue(82);
+q.print();
+q.enqueue(83);
+q.print();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
